0eb0a60309782fa9063fb64bd8e6f91087689f57
dogkentt <vahonin07@list.ru>
2026-02-17 13:29:44 +0300

    initial project (theme 2.1 ) & formed base structure & downloading all images & add local font & segmented scss files

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import '@/shared/assets/styles/index.scss'
+
+export const metadata: Metadata = {
+  title: "Hello world!",
+  description: "Hi",
+  authors: {
+    name: "Ilya Vahonin",
+    url: "https://github.com/Dogkentt/web-marsu",
+  }
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
